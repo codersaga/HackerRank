5e657a4a728e60bde444904239802ee5d47175c7
codersaga <64676906+codersaga@users.noreply.github.com>
2020-05-03 23:21:41 +0530

    Easy

---
+++ C/Introduction/Pointers in C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+    int aa = *a;
+    *a = *a+*b;
+    *b = abs(aa-*b);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
