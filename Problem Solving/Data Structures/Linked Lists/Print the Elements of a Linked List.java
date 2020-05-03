29d8cdbfdb6ed2bc4ee1e93c360d1962c5f0b18b
codersaga <64676906+codersaga@users.noreply.github.com>
2020-05-03 18:15:57 +0530

    Easy

---
+++ Problem Solving/Data Structures/Linked Lists/Print the Elements of a Linked List.java
@@ -0,0 +1,21 @@
+
+
+    // Complete the printLinkedList function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void printLinkedList(SinglyLinkedListNode head) {
+        while(head!=null){
+            System.out.println(head.data);
+            head=head.next;
+        }
+        
+    }
+
