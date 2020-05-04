e232c29e1b67d94de8e5a6bdc5dd3fabbc9689ed
codersaga <64676906+codersaga@users.noreply.github.com>
2020-05-04 18:39:07 +0530

    Create Day 0: Hello, World..java

---
+++ 30 Days of Code/Day 0: Hello, World..java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+      public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            String str=sc.nextLine();
+            System.out.println("Hello, World.");
+            System.out.println(str);
+      }
+}
